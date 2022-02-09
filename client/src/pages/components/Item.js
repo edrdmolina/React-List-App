bdbc338bed33d8f7fb143b2796f1c7a41de5fdb8
Eduardo Molina <edrdmolina11@gmail.com>
2022-02-09 12:38:02 -0500

    Add edit item form

--- client/src/pages/components/Item.js
+++ client/src/pages/components/Item.js
@@ -7,9 +7,52 @@ import 'swiper/swiper.min.css';
 import '../../styles/Item.css'
 
 class Item extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+        title: '',
+        quantity: 0,
+        edit: false,
+      }
+    }
+
+    componentDidMount() {
+        this.setState({
+            title: this.props.title,
+            quantity: this.props.quantity
+        })
+    }
+
     handleClick = e => {
         this.props.checkItem(e.target.id);
     }
+
+    handleEditToggle = () => {
+        this.setState(cs => ({
+            edit: !cs.edit,
+        }))
+    }
+
+    handleInputChange = e => {
+        this.setState({
+            title: e.target.value
+        })
+    }
+
+    handleQtyIncrement = () => {
+        this.setState(cs => ({
+            quantity: cs.quantity + 1
+        }))
+    }
+
+    handleQtyDecrement = () => {
+        if(this.state.quantity === 1) return;
+        this.setState(cs => ({
+            quantity: cs.quantity - 1
+        }))
+    }
+
     render() {
         const { _id, checked, title, quantity } = this.props
         return (
@@ -26,9 +69,24 @@ class Item extends Component {
                         </div>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide className='table-row'>
-                    edit
-                </SwiperSlide>
+                { this.state.edit ? (
+                    <SwiperSlide className='table-row-edit-form'>
+                        <input type='text' value={this.state.title} onChange={this.handleInputChange} />
+                        <div className='edit-qty'>
+                            <i className='far fa-minus-square minus' onClick={this.handleQtyDecrement} />
+                            <div className='qty'>{this.state.quantity}</div>
+                            <i className='far fa-plus-square plus' onClick={this.handleQtyIncrement} />
+                        </div>
+                        <button>SAVE</button>
+                    </SwiperSlide>
+                ) : (
+                    <SwiperSlide className='table-row-edit-btn'>
+                        <i className="far fa-trash-alt" />
+                        <i className="far fa-check-square" id={_id} onClick={this.handleClick} />
+                        <i className="far fa-edit" onClick={this.handleEditToggle} />
+                    </SwiperSlide>
+                ) }
+                
             </Swiper>
         )
     }
